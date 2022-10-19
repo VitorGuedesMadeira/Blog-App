7569480aa5c11b77993d485e2385e4c09829be09
VitorGuedesMadeira <vgm_rox@hotmail.com>
2022-10-19 13:10:28 -0300

    Fix linters

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ApplicationRecord
-    has_many :comments
-    has_many :likes
-    belongs_to :user
+  has_many :comments
+  has_many :likes
+  belongs_to :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-    has_many :comments
-    has_many :likes
-    has_many :posts
+  has_many :comments
+  has_many :likes
+  has_many :posts
 end
