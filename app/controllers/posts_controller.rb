8774923074695a7d4136b430661f7ab3e8330e45
VitorGuedesMadeira <vgm_rox@hotmail.com>
2022-10-26 17:05:20 -0300

    Add views

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,10 @@
 class PostsController < ApplicationController
-  def index; end
+  def index
+    @user = User.find(params[:user_id])
+  end
 
   def show
-    params[:id]
+    @user = User.find(params[:user_id])
+    @post = Post.find(params[:id])
   end
 end
