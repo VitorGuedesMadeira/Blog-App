bedf0f34fc9f1b450d879f2802d0103c62a45f9c
VitorGuedesMadeira <vgm_rox@hotmail.com>
2022-11-02 13:09:54 -0300

    Fix linters

--- Gemfile
+++ Gemfile
@@ -68,8 +68,8 @@ end
 group :development do
   # Use console on exceptions pages [https://github.com/rails/web-console]
   gem 'bullet'
-  gem 'web-console'
   gem 'letter_opener'
+  gem 'web-console'
   # Add speed badges [https://github.com/MiniProfiler/rack-mini-profiler]
   # gem "rack-mini-profiler"
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
-         
+
   has_many :comments
   has_many :likes
   has_many :posts
