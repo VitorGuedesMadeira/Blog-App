c1e9bea7e697474e1065943d8c2a006898e6cd06
VitorGuedesMadeira <vgm_rox@hotmail.com>
2022-11-03 11:38:18 -0300

    Fix bugs

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,7 +6,7 @@ class Post < ApplicationRecord
   belongs_to :user
 
   validates :title, presence: true, length: { maximum: 250 }
-  validates :text, presence: true, length: { maximum: 100 }
+  validates :text, presence: true, length: { maximum: 500 }
   validates :likes_counter, :comments_counter, numericality: { only_integer: true },
                                                comparison: { greater_than_or_equal_to: 0 }
 
